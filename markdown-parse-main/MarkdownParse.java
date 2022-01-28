45cadb2ea17b2a890ae828687d824c835000b8a0
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2022-01-28 13:32:01 -0800

    Update MarkdownParse.java

--- markdown-parse-main/MarkdownParse.java
+++ markdown-parse-main/MarkdownParse.java
@@ -16,6 +16,9 @@ public class MarkdownParse {
                 break;
             }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            if (nextCloseBracket == -1) {
+                break;
+            }
             int openParen = markdown.indexOf("(", nextCloseBracket);
             if (openParen == -1) {
                 break;
